fa8c9fd342c74d0c360589cd3a21d9de92dc8430
rezzycavalheiro <recavalheiro89@gmail.com>
2021-06-21 00:23:39 -0300

    changing style

--- app/src/main/java/com/example/betterreads/view/OrderPage.java
+++ app/src/main/java/com/example/betterreads/view/OrderPage.java
@@ -18,9 +18,6 @@ import java.util.List;
 
 public class OrderPage extends AppCompatActivity {
 
-    private TextView cleanLevelText;
-    private ProgressBar cleanLevelProgress;
-    private SeekBar cleanLevelSeekBar;
     List<CarPictures> carPaths;
     RecyclerView recyclerView;
     RecyclerViewAdapter adapter;
@@ -35,34 +32,6 @@ public class OrderPage extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         recyclerView.setAdapter(adapter);
 
-        cleanLevelText = findViewById(R.id.text_clean_level);
-        cleanLevelProgress = findViewById(R.id.progressBar_clean_level);
-        cleanLevelSeekBar = findViewById(R.id.seekBar_clean_level);
-
-        cleanLevelProgress.setMax(3);
-        cleanLevelSeekBar.setMax(2);
-
-        // SEEKBAR COM NO MÁXIMO 2 PONTOS DE PROGRESSO E
-        // PROGRESSBAR COM NO MÁXIMO VALOR 3
-        cleanLevelSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // SEEKBAR E PROGRESSBAR COMEÇANDO EM 1 (PRIMEIRO NÍVEL)
-                progress = progress + 1;
-                cleanLevelProgress.setProgress(progress);
-                cleanLevelText.setText("" + progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
     }
 
     // BOTÃO PARA ADICIONAR FOTOS
@@ -70,19 +39,7 @@ public class OrderPage extends AppCompatActivity {
         Intent intent = new Intent(this, CameraPage.class);
         startActivity(intent);
     }
-
-    // BOTÃO PARA ENVIAR PEDIDO
-    public void sendOrderButton(View view){
-        Intent intent = new Intent(this, OrderSent.class);
-        startActivity(intent);
-    }
-
-    // POP UP DE INFORMAÇÕES
-    public void popUpInforButton(View view){
-        Intent intent = new Intent(this, PopUpInfo.class);
-        startActivity(intent);
-    }
-
+    
     // BOTÃO PARA VOLTAR PARA TELA DE MENU
     public void menuPageButton(View view){
         Intent intent = new Intent(this, StartMenu.class);
