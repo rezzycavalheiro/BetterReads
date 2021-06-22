856ab24131d56273ea8634ff55ad73307ec43d41
rezzycavalheiro <recavalheiro89@gmail.com>
2021-06-22 01:34:59 -0300

    adding crashytics

--- app/src/main/java/com/example/betterreads/view/MainActivity.java
+++ app/src/main/java/com/example/betterreads/view/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.betterreads.R;
 
@@ -14,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TESTE DE CRASH PARA O CRASHLYTICS
+        // COMENTAR APÓS APRESENTAÇÃO DE CRASH
+//        Button crashButton = new Button(this);
+//        crashButton.setText("Crash!");
+//        crashButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                throw new RuntimeException("Test Crash"); // Force a crash
+//            }
+//        });
+//
+//        addContentView(crashButton, new ViewGroup.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT));
+        //
     }
 
     public void loginButton(View view){
